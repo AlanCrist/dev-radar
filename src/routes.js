cb011f261848f691a15f0661d6f7f97eb92d2a0f
alancrist <alancristian14lima@gmail.com>
2020-01-18 00:12:35 -0300

    add controller dev

--- src/routes.js
+++ src/routes.js
@@ -1,17 +1,10 @@
 const { Router } = require("express");
 const axios = require("axios");
+const Dev = require("./models/Dev");
 
 const routes = Router();
 
 
-routes.post("/devs", async (req, res) => {
-    const { github_username } = req.body;
-
-    apiResp = await axios.get(`http://api.github.com/users/${github_username}`)
-
-    console.log(apiResp.data);
-
-    return res.json({ message: "Hello" });
-});
+routes.post("/devs");
 
 module.exports = routes;
