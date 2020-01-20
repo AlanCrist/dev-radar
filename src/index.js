b77f707aa6a2d9d481261c3d28a55593576eca22
Alan <lanzn16@outlook.com>
2020-01-19 22:07:44 -0300

    add protocol http socket.io

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const routes = require("./routes");
+const http = require('http');
 const cors = require("cors")
+const { setupWebsocket } = require("./websocket")
+
 require("dotenv/config");
 
 const app = express();
+const server = http.Server(app);
+
+setupWebsocket(server);
 
 mongoose.connect(`mongodb+srv://${process.env.DB_LOGIN}:${process.env.DB_PASSWORD}@cluster0-hrys0.mongodb.net/week10?retryWrites=true&w=majority`, { useNewUrlParser: true, useUnifiedTopology: true });
 app.use(cors({}));
@@ -12,4 +18,4 @@ app.use(express.json());
 app.use(routes);
 
 
-app.listen(4000);
+server.listen(4000);
