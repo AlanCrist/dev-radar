70ae34ee6a7b77c66e9bc53d99ae6262d36a192b
Alan <lanzn16@outlook.com>
2020-01-19 21:38:04 -0300

    add cors in project

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const routes = require("./routes");
+const cors = require("cors")
 require("dotenv/config");
 
 const app = express();
 
 mongoose.connect(`mongodb+srv://${process.env.DB_LOGIN}:${process.env.DB_PASSWORD}@cluster0-hrys0.mongodb.net/week10?retryWrites=true&w=majority`, { useNewUrlParser: true, useUnifiedTopology: true });
-
+app.use(cors({}));
 app.use(express.json());
 app.use(routes);
 
